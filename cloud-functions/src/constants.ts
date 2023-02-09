d3f6c3fc15196d5ad8975488d352c4f2fb88997a
alphainfinitus <kumar.mridul000@gmail.com>
2023-02-09 22:55:28 +0530

    address validation

--- cloud-functions/src/constants.ts
+++ cloud-functions/src/constants.ts
@@ -1,6 +1,6 @@
 export const responseMessages = {
 	missing_params: 'Missing parameters.',
-	invalid_argument: 'Invalid parameters passed to the function call.',
+	invalid_params: 'Invalid parameters passed to the function call.',
 	invalid_signature: 'Invalid signature.',
 	internal: 'Internal error occured.'
 };
