3fae1c508989369ff6cc9a0c5892a591e04d2535
alphainfinitus <kumar.mridul000@gmail.com>
2023-03-11 01:04:45 +0530

    firebase functions url fix

--- src/global/firebaseFunctionsUrl.ts
+++ src/global/firebaseFunctionsUrl.ts
@@ -2,4 +2,4 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export const FIREBASE_FUNCTIONS_URL = process.env.REACT_APP_ENV === 'dev' ? process.env.REACT_APP_FIREBASE_FUNCTIONS_URL_DEV : process.env.REACT_APP_FIREBASE_FUNCTIONS_URL ;
+export const FIREBASE_FUNCTIONS_URL = process.env.REACT_APP_ENV === 'dev' ? process.env.REACT_APP_FIREBASE_FUNCTIONS_URL_DEV : 'https://us-central1-polkasafe-a8042.cloudfunctions.net' ;
