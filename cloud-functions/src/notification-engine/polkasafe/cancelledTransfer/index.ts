fb00e232e1f1128b803a0b91ef3e4fd3289486b4
alphainfinitus <kumar.mridul000@gmail.com>
2023-05-20 23:49:13 +0530

    cancelledTransfer

--- cloud-functions/src/notification-engine/polkasafe/cancelledTransfer/index.ts
+++ cloud-functions/src/notification-engine/polkasafe/cancelledTransfer/index.ts
@@ -1,25 +1,31 @@
 import { NotificationService } from '../../NotificationService';
 import getSourceFirebaseAdmin from '../../global-utils/getSourceFirebaseAdmin';
 import getSubstrateAddress from '../../global-utils/getSubstrateAddress';
-import { IUserNotificationPreferences, NOTIFICATION_SOURCE } from '../../notification_engine_constants';
+import { NOTIFICATION_SOURCE } from '../../notification_engine_constants';
 import getTemplateRender from '../../global-utils/getTemplateRender';
 import getTriggerTemplate from '../../global-utils/getTriggerTemplate';
 import isValidTemplateArgs from '../../global-utils/isValidTemplateArgs';
+import getMultisigData from '../_utils/getMultisigData';
+import getPSUser from '../_utils/getPSUser';
+import { IPSMultisigSettings } from '../_utils/types';
 
 const TRIGGER_NAME = 'cancelledTransfer';
 const SOURCE = NOTIFICATION_SOURCE.POLKASAFE;
 
 interface Args {
+	address: string;
 	network: string;
 	addresses: string[];
 	callHash: string;
+	multisigAddress: string;
 }
 
 export default async function cancelledTransfer(args: Args) {
 	if (!args) throw Error(`Missing arguments for trigger: ${TRIGGER_NAME}`);
 
-	const { network, addresses, callHash } = args;
-	if (!network || !addresses || !callHash || !Array.isArray(addresses) || !addresses.length) throw Error(`Invalid arguments for trigger: ${TRIGGER_NAME}`);
+	const { address, network, addresses, callHash, multisigAddress } = args;
+	const substrateAddress = getSubstrateAddress(address);
+	if (!substrateAddress || !network || !addresses || !callHash || !Array.isArray(addresses) || !addresses.length) throw Error(`Invalid arguments for trigger: ${TRIGGER_NAME}`);
 
 	// convert to substrateAddresses and dedupe
 	const substrateAddresses: string[] = addresses.reduce((acc, curr) => {
@@ -35,8 +41,13 @@ export default async function cancelledTransfer(args: Args) {
 	for (const address of substrateAddresses) {
 		const addressDoc = await firestore_db.collection('addresses').doc(address).get();
 		const addressData = addressDoc?.data();
+
 		if (addressData) {
-			const userNotificationPreferences: IUserNotificationPreferences = addressData.notification_preferences;
+			const { name: defaultMultisigName } = await getMultisigData(firestore_db, multisigAddress);
+			const { multisigSettings, notification_preferences } = await getPSUser(firestore_db, substrateAddress);
+			const { deleted, name: userMultisigName } = multisigSettings[multisigAddress] as IPSMultisigSettings;
+
+			if (deleted || !notification_preferences) continue; // skip if multisig is deleted or user has no notification preferences
 
 			const triggerTemplate = await getTriggerTemplate(firestore_db, SOURCE, TRIGGER_NAME);
 			if (!triggerTemplate) throw Error(`Template not found for trigger: ${TRIGGER_NAME}`);
@@ -44,7 +55,10 @@ export default async function cancelledTransfer(args: Args) {
 			if (triggerTemplate.args.length > 0 && !isValidTemplateArgs(args, triggerTemplate.args)) throw Error(`Invalid arguments for trigger template : ${TRIGGER_NAME}`);
 
 			const subject = triggerTemplate.subject;
-			const { htmlMessage, textMessage } = getTemplateRender(triggerTemplate.template, args);
+			const { htmlMessage, textMessage } = getTemplateRender(triggerTemplate.template, {
+				...args,
+				multisigName: userMultisigName || defaultMultisigName
+			});
 
 			const notificationServiceInstance = new NotificationService(
 				SOURCE,
@@ -56,7 +70,7 @@ export default async function cancelledTransfer(args: Args) {
 					network
 				}
 			);
-			notificationServiceInstance.notifyAllChannels(userNotificationPreferences);
+			notificationServiceInstance.notifyAllChannels(notification_preferences);
 		}
 	}
 }
