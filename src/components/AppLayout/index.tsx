96f84fed09e3de44f32fedaf248779db63087fb9
Hitesh-gurnani <hiteshgurnani850@gmail.com>
2023-06-01 03:09:52 +0530

    added slider

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -1,9 +1,10 @@
 // Copyright 2022-2023 @Polkasafe/polkaSafe-ui authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
 import { Drawer, Layout } from 'antd';
 import React, { useEffect, useState } from 'react';
+import longiframe from 'src/assets/longiframe.svg';
+import shortiframe from 'src/assets/shortiframe.svg';
 import { useGlobalApiContext } from 'src/context/ApiContext';
 import { useGlobalUserDetailsContext } from 'src/context/UserDetailsContext';
 import Loader from 'src/ui-components/Loader';
@@ -26,7 +27,8 @@ const AppLayout = ({ className }: { className?: string }) => {
 	const [multisigChanged, setMultisigChanged] = useState(false);
 	const { activeMultisig } = useGlobalUserDetailsContext();
 	const { iframeVisibility } = useGlobalApiContext();
-	const [IframeUrl,setIframeUrl]=useState('');
+	const [IframeUrl, setIframeUrl] = useState('');
+	const [iframestate, setiframestate] = useState(false);
 	useEffect(() => {
 		setMultisigChanged(true);
 		setTimeout(() => {
@@ -39,28 +41,33 @@ const AppLayout = ({ className }: { className?: string }) => {
 		<Layout className={className}>
 			<NavHeader setSideDrawer={setSideDrawer} sideDrawer={sideDrawer} />
 			<Layout hasSider>
-				<Sider
-					trigger={null}
-					collapsible={false}
-					collapsed={true}
-					className={'hidden overflow-y-hidden bg-bg-main sidebar lg:block top-0 bottom-0 left-0 h-screen fixed z-40 w-full max-w-[180px]'}
-				>
-					<Menu />
-				</Sider>
-				<Drawer
-					placement='left'
-					closable={false}
-					onClose={() => setSideDrawer(false)}
-					open={sideDrawer}
-					getContainer={false}
-					className='w-full max-w-[180px] p-0'
-				>
-					<Menu />
-				</Drawer>
-				<Layout className='min-h flex flex-row p-0 bg-bg-main'>
+				{
+
+					iframeVisibility && iframestate ? <div className='w-full '><img src={shortiframe} alt="" width="30" height="30" className='cursor-pointer absolute top-1/2 transform -translate-y-1/2 z-10 ' onClick={() => setiframestate(false)} /><iframe src={IframeUrl} className='w-full relative ml-8' height={window.innerHeight}></iframe></div> : <>
+						<Sider
+							trigger={null}
+							collapsible={false}
+							collapsed={true}
+							className={'hidden overflow-y-hidden bg-bg-main sidebar lg:block top-0 bottom-0 left-0 h-screen fixed w-full max-w-[180px]'}
+						>
+							<Menu />
+						</Sider>
+						<Drawer
+							placement='left'
+							closable={false}
+							onClose={() => setSideDrawer(false)}
+							open={sideDrawer}
+							getContainer={false}
+							className='w-full max-w-[180px] p-0'
+						>
+							<Menu />
+						</Drawer>
+					</>
+				}
+				<Layout className='min-h flex flex-row p-0 bg-bg-main '>
 					<div className='hidden lg:block w-full max-w-[180px]'></div>
 					{
-						iframeVisibility && IframeUrl && window.location.pathname.split('/').pop() == 'apps' ? <iframe src={IframeUrl} width={window.innerWidth} height={window.innerHeight}/>:<><Content className='bg-bg-secondary p-[30px] rounded-lg'>
+						iframeVisibility && IframeUrl && !iframestate && window.location.pathname.split('/').pop() == 'apps' ? <div className='w-full '><iframe src={IframeUrl} className='w-full' height={window.innerHeight}></iframe><img src={longiframe} alt="" width="30" height="30" className='cursor-pointer absolute top-1/2 left-auto -ml-4 transform -translate-y-1/2 z-9999' onClick={() => setiframestate(true)} /></div> : <><Content className='bg-bg-secondary p-[30px] rounded-lg'>
 							{multisigChanged ? <Loader size='large' /> : <SwitchRoutes />}
 						</Content></>
 					}
