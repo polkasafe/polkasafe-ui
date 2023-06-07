319e5bf0053310e645760c6ef5e950485dbafb44
aadarsh012 <aadarsh012@gmail.com>
2023-06-08 01:35:30 +0530

    verify email bug fix

--- src/Screens/VerifyEmailToken/index.tsx
+++ src/Screens/VerifyEmailToken/index.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useNavigate,useSearchParams } from 'react-router-dom';
 import { useGlobalApiContext } from 'src/context/ApiContext';
 import { useGlobalUserDetailsContext } from 'src/context/UserDetailsContext';
@@ -17,59 +17,53 @@ const VerifyEmailToken = () => {
 	const navigate = useNavigate();
 	const { network } = useGlobalApiContext();
 	const { setUserDetailsContextState } = useGlobalUserDetailsContext();
-	const [verified, setVerified] = React.useState<boolean>(false);
 
-	useEffect(() => {
+	const verifyEmail = useCallback(async () => {
 		const email = searchParams.get('email');
 		const token = searchParams.get('token');
-		const verifyEmail = async () => {
-			const verifyEmailRes = await fetch(`${FIREBASE_FUNCTIONS_URL}/verifyEmail`, {
-				body: JSON.stringify({
-					email,
-					token
-				}),
-				headers: firebaseFunctionsHeader(network),
-				method: 'POST'
+		const verifyEmailRes = await fetch(`${FIREBASE_FUNCTIONS_URL}/verifyEmail`, {
+			body: JSON.stringify({
+				email,
+				token
+			}),
+			headers: firebaseFunctionsHeader(network),
+			method: 'POST'
+		});
+
+		const { data: verifyEmailData, error: verifyEmailError } = await  verifyEmailRes.json() as { data: string, error: string };
+
+		if(verifyEmailError) {
+			console.log(verifyEmailError);
+			return;
+		}
+
+		if(verifyEmailData){
+			queueNotification({
+				header: 'Success!',
+				message: 'Your Email has been verified.',
+				status: NotificationStatus.SUCCESS
 			});
-
-			const { data: verifyEmailData, error: verifyEmailError } = await  verifyEmailRes.json() as { data: string, error: string };
-
-			if(verifyEmailError) {
-				console.log(verifyEmailError);
-				return;
-			}
-
-			if(verifyEmailData){
-				setVerified(true);
-				setUserDetailsContextState(prev => ({
-					...prev,
-					notification_preferences: {
-						...prev.notification_preferences,
-						channelPreferences: {
-							...prev.notification_preferences.channelPreferences,
-							[CHANNEL.EMAIL]:{
-								...prev.notification_preferences.channelPreferences[CHANNEL.EMAIL],
-								verified: true
-							}
+			setUserDetailsContextState(prev => ({
+				...prev,
+				notification_preferences: {
+					...prev.notification_preferences,
+					channelPreferences: {
+						...prev.notification_preferences.channelPreferences,
+						[CHANNEL.EMAIL]:{
+							...prev.notification_preferences.channelPreferences[CHANNEL.EMAIL],
+							verified: true
 						}
 					}
-				}));
-				navigate('/notification-settings', { state: { emailVerified: true } });
-			}
-		};
-		verifyEmail();
-		return () => {
-			if(verified){
-				console.log('Email Verified');
-				queueNotification({
-					header: 'Success!',
-					message: 'Your Email has been verified.',
-					status: NotificationStatus.SUCCESS
-				});
-			}
-		};
+				}
+			}));
+			navigate('/notification-settings');
+		}
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
+
+	useEffect(() => {
+		verifyEmail();
+	}, [verifyEmail]);
 
 	return (
 		<div className='h-[70vh] bg-bg-main rounded-lg m-auto flex items-center justify-center'>
