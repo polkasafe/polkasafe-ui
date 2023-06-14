bd4ec72a0723581f168a406a8feba29295ec78b4
alphainfinitus <kumar.mridul000@gmail.com>
2023-06-14 20:58:49 +0530

    remove footer from html template container

--- cloud-functions/src/notification-engine/global-utils/html-template-container.ts
+++ cloud-functions/src/notification-engine/global-utils/html-template-container.ts
@@ -1,21 +1,17 @@
 import { NOTIFICATION_SOURCE } from '../notification_engine_constants';
 
-export const emailTemplateContainer = (source: NOTIFICATION_SOURCE, content: string): string => {
-	let privacyPolicyLink = '';
+export const htmlTemplateContainer = (source: NOTIFICATION_SOURCE, content: string): string => {
 	let notificationSettingsLink = '';
 
 	switch (source) {
 	case NOTIFICATION_SOURCE.POLKASSEMBLY:
-		privacyPolicyLink = 'https://polkadot.polkassembly.io/privacy';
 		notificationSettingsLink = 'https://polkadot.polkassembly.io/settings?tab=notifications';
 		break;
 	case NOTIFICATION_SOURCE.POLKASAFE:
-		privacyPolicyLink = 'https://app.polkasafe.xyz/privacy-policy';
 		notificationSettingsLink = 'https://app.polkasafe.xyz/notification-settings';
 		break;
 		// TODO: fix townhall links
 	case NOTIFICATION_SOURCE.TOWNHALL:
-		privacyPolicyLink = 'https://townhallgov.com/policy/privacy-policy/';
 		notificationSettingsLink = 'https://townhallgov.com/notification-settings';
 		break;
 	}
@@ -37,28 +33,18 @@ export const emailTemplateContainer = (source: NOTIFICATION_SOURCE, content: str
 					text-decoration: none;
 				}
 				p { margin: 0; padding: 0; }
-				.polk-container {
+				.content-container {
 					margin: 0 auto;
 					max-width: 600px;
-				}
-				.footer {
-					margin-top: 20px;
-					font-size: 10px;
-					text-align: center;
-					color: #313638;
 				}
 			</style>
 		</head>
 		<body>
-			<div class="polk-container">
+			<div class="content-container">
 				${content}
 				<br/><br/>
-				You can deactivate this notification in your <a href="${notificationSettingsLink}">notification settings</a>.<br /><br />
+				You can deactivate this notification in your <a href="${notificationSettingsLink}">notification settings</a>.<br />
 				${source.charAt(0).toUpperCase()}${source.slice(1)} Team
-			</div>
-			<div class="footer">
-				Registered Address: Polka Labs PTE. LTD., 3 Fraser Street, #05-25, Duo Tower, Singapore (189352)<br />
-				<a target="_blank" href="${privacyPolicyLink}">Privacy Policy</a>
 			</div>
 		</body>
 	</html>
