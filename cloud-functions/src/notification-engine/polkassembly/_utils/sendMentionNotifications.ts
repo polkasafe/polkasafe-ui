fe1f5173c1697ae9863a98751006192577e4834a
alphainfinitus <kumar.mridul000@gmail.com>
2023-06-30 08:05:27 +0530

    new getTemplateRender implementation for newMention trigger

--- cloud-functions/src/notification-engine/polkassembly/_utils/sendMentionNotifications.ts
+++ cloud-functions/src/notification-engine/polkassembly/_utils/sendMentionNotifications.ts
@@ -1,6 +1,5 @@
 import { NotificationService } from '../../NotificationService';
 import getTemplateRender from '../../global-utils/getTemplateRender';
-import getTriggerTemplate from '../../global-utils/getTriggerTemplate';
 import { NOTIFICATION_SOURCE } from '../../notification_engine_constants';
 import getMentionedUsernames from './getMentionedUsernames';
 import getNetworkNotificationPrefsFromPANotificationPrefs from './getNetworkNotificationPrefsFromPANotificationPrefs';
@@ -27,9 +26,6 @@ export default async function sendMentionNotifications(args : Args) {
 	console.log(`Mentioned usernames: ${JSON.stringify(mentionedUsernames)}`);
 	if (!mentionedUsernames.length) return;
 
-	const triggerTemplate = await getTriggerTemplate(firestore_db, SOURCE, TRIGGER_NAME);
-	if (!triggerTemplate) throw Error(`Template not found for trigger: ${TRIGGER_NAME}`);
-
 	for (const mentionedUsername of mentionedUsernames) {
 		// get user preferences
 		const mentionedUserDocSnapshot = await firestore_db.collection('users').where('username', '==', mentionedUsername).limit(1).get();
@@ -43,16 +39,18 @@ export default async function sendMentionNotifications(args : Args) {
 
 		if (!(mentionedUserNotificationPreferences.triggerPreferences?.[TRIGGER_NAME]?.mention_types || []).includes(type)) continue;
 
-		const subject = triggerTemplate.subject;
-		const { htmlMessage, markdownMessage, textMessage } = getTemplateRender(triggerTemplate.template, {
-			...args,
-			authorUsername,
-			url,
-			content: htmlContent,
-			domain: `https://${network}.polkassembly.io`,
-			username: mentionedUsername,
-			mentionType: type
-		});
+		const { htmlMessage, markdownMessage, textMessage, subject } = await getTemplateRender(
+			SOURCE,
+			TRIGGER_NAME,
+			{
+				...args,
+				authorUsername,
+				url,
+				content: htmlContent,
+				domain: `https://${network}.polkassembly.io`,
+				username: mentionedUsername,
+				mentionType: type
+			});
 
 		const notificationServiceInstance = new NotificationService(
 			SOURCE,
