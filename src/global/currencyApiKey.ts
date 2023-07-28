c3ba770e23a23af8b55fafd1acfcfe56a62ef3f3
Aadarsh Shaw <aadarshshaw@Aadarshs-Air.Dlink>
2023-07-28 21:57:26 +0530

    api key fix

--- src/global/currencyApiKey.ts
+++ src/global/currencyApiKey.ts
@@ -2,4 +2,4 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export const CURRENCY_API_KEY = process.env.REACT_APP_ENV === 'dev' ? process.env.REACT_APP_CURRENCY_API_KEY_DEV : process.env.REACT_APP_CURRENCY_API_KEY_PROD ;
+export const CURRENCY_API_KEY = process.env.REACT_APP_ENV === 'dev' ? 'cur_live_8NlGKWBefFI9XHVAIgcWweO5AjhEMUr8oqJFrmra' : 'cur_live_ZGW68myNKB7GoRvr86Ft6qYtDPMC1gkBolKl5DLw' ;
