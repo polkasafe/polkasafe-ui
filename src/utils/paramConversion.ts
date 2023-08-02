80d507c29c2b980b2db4916dd0b2fda2ff0345e3
Aadarsh Shaw <aadarshshaw@Aadarshs-MacBook-Air.local>
2023-08-03 00:28:16 +0530

    added manual extrinsic

---
+++ src/utils/paramConversion.ts
@@ -0,0 +1,21 @@
+// Copyright 2022-2023 @Polkasafe/polkaSafe-ui authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+const paramConversion = {
+	num: [
+		'Compact<Balance>',
+		'BalanceOf',
+		'u8',
+		'u16',
+		'u32',
+		'u64',
+		'u128',
+		'i8',
+		'i16',
+		'i32',
+		'i64',
+		'i128'
+	]
+};
+
+export default paramConversion;
