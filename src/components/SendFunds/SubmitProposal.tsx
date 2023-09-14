6ef7bf1f656202695b25244b6435e29b919d36f6
aadarsh012 <aadarsh012@gmail.com>
2023-09-14 13:28:57 +0530

    added block number at block and after delay

--- src/components/SendFunds/SubmitProposal.tsx
+++ src/components/SendFunds/SubmitProposal.tsx
@@ -2,8 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 import { BN_HUNDRED } from '@polkadot/util';
-import { Dropdown, Form, Input } from 'antd';
+import { Dropdown, Form, Input, Radio } from 'antd';
 import { ItemType } from 'antd/lib/menu/hooks/useItems';
+import BN from 'bn.js';
 import React, { useEffect, useState } from 'react';
 import { useGlobalApiContext } from 'src/context/ApiContext';
 import { networkTrackInfo } from 'src/global/networkTrackInfo';
@@ -16,6 +17,8 @@ const SubmitProposal = ({ className, setCallData }: { className?: string, setCal
 	const [preimageHash, setPreimageHash] = useState<string>('');
 	const [preimageLength, setPreimageLength] = useState<number>(0);
 	const [track, setTrack] = useState('');
+	const [enactment, setEnactment] = useState(0);
+	const [blockNumber, setBlockNumber] = useState(100);
 
 	const trackArr: string[] = [];
 
@@ -33,18 +36,20 @@ const SubmitProposal = ({ className, setCallData }: { className?: string, setCal
 	}));
 
 	useEffect(() => {
-		if(!api || !apiReady || !api.tx.referenda || !api.tx.referenda.submit || !preimageHash || !track || !preimageLength) {
+		if(!api || !apiReady || !api.tx.referenda || !api.tx.referenda.submit || !preimageHash || !track || !preimageLength || !blockNumber) {
 			setCallData('');
 			return;
 		}
 
 		const origin: any = { Origins: track };
 
-		const proposal = api.tx.referenda.submit(origin ,{ Lookup: { hash: preimageHash, len: String(preimageLength) } },  { After: BN_HUNDRED });
+		const bnBlockNumber = new BN(blockNumber || '0');
+
+		const proposal = api.tx.referenda.submit(origin ,{ Lookup: { hash: preimageHash, len: String(preimageLength) } }, bnBlockNumber ? enactment === 1 ? { At: bnBlockNumber } : { After: bnBlockNumber } : { After: BN_HUNDRED });
 		if(proposal) setCallData(proposal.method.toHex());
 
 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [api, apiReady, preimageHash, preimageLength, track]);
+	}, [api, apiReady, preimageHash, preimageLength, track, blockNumber, enactment]);
 
 	return (
 		<div className={className}>
@@ -113,6 +118,46 @@ const SubmitProposal = ({ className, setCallData }: { className?: string, setCal
 						</div>
 					</Dropdown>
 				</Form.Item>
+			</section>
+			<section className='mt-[15px] w-[500px]'>
+				<div className='flex items-center gap-x-[10px]'>
+					<article className='w-full'>
+						<Form.Item
+							className='border-0 outline-0 my-0 p-0'
+							name="block-radio"
+							rules={[{ message: 'Required', required: true }]}
+						>
+							<div className='flex items-center'>
+								<Radio.Group value={enactment} onChange={(e) => setEnactment(e.target.value)}>
+									<Radio className='text-primary [&>span>span]:border-primary' value={0}>After Delay</Radio>
+									<Radio className='text-primary [&>span>span]:border-primary' value={1}>At Block</Radio>
+								</Radio.Group>
+							</div>
+						</Form.Item>
+					</article>
+				</div>
+			</section>
+			<section className='mt-[15px] w-[500px]'>
+				<label className='text-primary font-normal text-xs leading-[13px] block mb-[5px]'>{enactment === 1 ? 'At Specific Block*' : 'After Number of Blocks*'}</label>
+				<div className='flex items-center gap-x-[10px]'>
+					<article className='w-full'>
+						<Form.Item
+							className='border-0 outline-0 my-0 p-0'
+							name="block-number"
+							rules={[{ message: 'Required', required: true }]}
+						>
+							<div className='flex items-center h-[50px]'>
+								<Input
+									id="block-number"
+									onChange={(a) => setBlockNumber(Number(a.target.value))}
+									placeholder='Preimage Hash'
+									value={blockNumber}
+									className="w-full h-full text-sm font-normal leading-[15px] border-0 outline-0 p-3 placeholder:text-[#505050] bg-bg-secondary rounded-lg text-white pr-20"
+								/>
+							</div>
+						</Form.Item>
+					</article>
+				</div>
 			</section>
 		</div>
 	);
