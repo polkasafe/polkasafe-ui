9de5e63ada39061430e5302919c4525942a011fa
alphainfinitus <kumar.mridul000@gmail.com>
2024-02-15 21:07:18 +0530

    feat: :sparkles: houseProfileUpdated trigger for townhall

--- cloud-functions/src/notification-engine/townhall/_utils/thFirestoreRefs.ts
+++ cloud-functions/src/notification-engine/townhall/_utils/thFirestoreRefs.ts
@@ -3,3 +3,6 @@ export const thPostRef = (firestore_db: FirebaseFirestore.Firestore, postId: str
 
 export const thUserRef = (firestore_db: FirebaseFirestore.Firestore, userId: string) =>
 	firestore_db.collection('users').doc(String(userId));
+
+export const thHouseRef = (firestore_db: FirebaseFirestore.Firestore, postId: string) =>
+	firestore_db.collection('houses').doc(String(postId));
